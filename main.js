a6ce6875c501c8a4502a71b0f1494408c723a06e
Nikhil Chauhan <91124741+nikhil786chauhan@users.noreply.github.com>
2023-06-03 14:16:41 +0530

    Random-Color_generator

---
+++ main.js
@@ -0,0 +1,12 @@
+const getcolor=()=>{
+    const randomcolor=Math.floor(Math.random()*16777215)
+    const randomcode='#'+ randomcolor.toString(16)
+    console.log(randomcolor,randomcode);
+    document.body.style.backgroundColor=randomcode;
+    document.getElementById('color-code').innerText=randomcode;
+}
+document.getElementById('btn').addEventListener(
+    "click",
+    getcolor
+)
+getcolor();
